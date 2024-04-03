a115f9a03de5900362c60cf03e574c79c335ec92
Will Richardson <william.hamish@gmail.com>
2024-04-03 04:59:41 +0000

    Migrate to use podman library

--- src/endash/container.cr
+++ src/endash/container.cr
@@ -1,88 +1,9 @@
 require "json"
+require "podman"
 
 class String
   def truncated
     self[..12]
-  end
-end
-
-enum Podman::State
-  Configured
-  Created
-  Running
-  Paused
-  Stopped
-  Exited
-end
-
-class Podman::Container
-  include JSON::Serializable
-
-  @[JSON::Field(key: "Id")]
-  getter id : String
-  @[JSON::Field(key: "Image")]
-  getter image : String
-  @[JSON::Field(key: "ImageID")]
-  getter image_id : String
-  @[JSON::Field(key: "Names")]
-  getter names : Array(String)
-
-  def name
-    @names.first.not_nil!
-  end
-
-  @[JSON::Field(key: "StartedAt", converter: Time::EpochConverter)]
-  getter started_at : Time
-  @[JSON::Field(key: "AutoRemove")]
-  getter auto_remove : Bool
-
-  @[JSON::Field(key: "ExitCode")]
-  getter exit_code : Int32
-  @[JSON::Field(key: "ExitedAt", converter: Time::EpochConverter)]
-  getter exited_at : Time
-
-  @[JSON::Field(key: "Networks")]
-  getter networks : Set(String)
-
-  def uptime_or_downtime : Time::Span
-    if @state.exited?
-      self.downtime
-    else
-      self.uptime
-    end
-  end
-
-  def downtime
-    Time.utc - @exited_at
-  end
-
-  def uptime : Time::Span
-    Time.utc - @started_at
-  end
-
-  @[JSON::Field(key: "State")]
-  getter state : State
-
-  @[JSON::Field(key: "Labels")]
-  getter _labels : Hash(String, String)?
-
-  struct Port
-    include JSON::Serializable
-    @[JSON::Field(key: "host_port")]
-    getter host_port : Int32
-    @[JSON::Field(key: "container_port")]
-    getter container_port : Int32
-  end
-
-  @[JSON::Field(key: "Ports")]
-  getter _ports : Array(Port)?
-
-  def labels
-    @_labels ||= Hash(String, String).new
-  end
-
-  def ports
-    @_ports ||= Array(Port).new
   end
 end
 
--- src/endash/image.cr
+++ src/endash/image.cr
@@ -1,18 +1,5 @@
 require "json"
-
-class Podman::Image
-  include JSON::Serializable
-  @[JSON::Field(key: "Id")]
-  getter id : String
-  @[JSON::Field(key: "Containers")]
-  getter containers : Int32
-
-  @[JSON::Field(key: "Created", converter: Time::EpochConverter)]
-  getter created_at : Time
-
-  @[JSON::Field(key: "Names")]
-  getter names = Array(String).new
-end
+require "podman"
 
 class EnDash::Image
   def initialize(@host : EnDash::Host, @image : Podman::Image)
